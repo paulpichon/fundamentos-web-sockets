9dacb42b348e7debdf0bee118b4cf63120282391
paul pichon <paul10_barca@hotmail.com>
2023-03-06 16:44:19 -0600

    Mensajes de conexion y desconexion - Cliente

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,3 +1,33 @@
+//referencias del HTML
+const lblOnline = document.querySelector('#lblOnline');
+const lblOffline = document.querySelector('#lblOffline');
+
+
+
 
 //socket del cliente
 const socket = io();
+
+//listeners -> eventlister
+//saber cuando alguien se conecta al servidor
+socket.on('connect', () => {
+    console.log('conectado');
+
+    //ocultar lblOffline
+    lblOffline.style.display = 'none';
+    //
+    lblOnline.style.display = '';
+
+});
+//saber cuando alguien se desconecta al servidor
+socket.on('disconnect', () => {
+    console.log('desconectado del servidor');
+
+
+
+    //ocultar lblOnline
+    lblOnline.style.display  = 'none';
+    //
+    lblOffline.style.display = '';
+
+});
